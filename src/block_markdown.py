30640291c3b3c5ad84a8e25403f64198320611ea
Priyanshu Gupta <priyanshoon.pg@gmail.com>
2026-05-15 19:24:07 +0530

    more shit ton of code

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,3 +1,14 @@
+from enum import Enum
+
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
 def markdown_to_blocks(markdown):
     if len(markdown) == 0:
         return []
@@ -12,3 +23,35 @@ def markdown_to_blocks(markdown):
             result.append(block_strip)
 
     return result
+
+def block_to_block_type(block):
+    lines = block.split("\n")
+
+    if block.startswith(("# ", "## ", "### ", "#### ", "##### ", "###### ")):
+        return BlockType.HEADING
+    
+    if len(lines) > 1 and lines[0].startswith("```") and lines[-1].startswith("```"):
+        return BlockType.CODE
+    
+    if block.startswith(">"):
+        for line in lines:
+            if not line.startswith(">"):
+                return BlockType.PARAGRAPH
+
+        return BlockType.QUOTE
+
+    if block.startswith("- "):
+        for line in lines:
+            if not line.startswith("- "):
+                return BlockType.PARAGRAPH
+        return BlockType.UNORDERED_LIST
+
+    if block.startswith("1. "):
+        i = 1
+        for line in lines:
+            if not line.startswith(f"{i}. "):
+                return BlockType.ORDERED_LIST
+            i += 1
+        return BlockType.ORDERED_LIST
+
+    return BlockType.PARAGRAPH
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -1,6 +1,6 @@
 import unittest
 
-from block_markdown import markdown_to_blocks
+from block_markdown import markdown_to_blocks, block_to_block_type, BlockType
 
 
 class TestBlockMarkdown(unittest.TestCase):
@@ -34,7 +34,7 @@ This is the same paragraph on a new line
         md = """
 
 
-        This is **bolded** paragraph
+This is **bolded** paragraph
 
 This is another paragraph with _italic_ text and `code` here
 This is the same paragraph on a new line
@@ -45,7 +45,7 @@ This is the same paragraph on a new line
 
 
 
-something"""
+>something"""
         blocks = markdown_to_blocks(md)
         self.assertEqual(
             blocks,
@@ -53,6 +53,59 @@ something"""
                 "This is **bolded** paragraph",
                 "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line",
                 "- This is a list\n- with items",
-                "something",
+                ">something",
             ],
         )
+
+    def test_block_to_block_type_paragraph(self):
+        paragraph = "this is paragraph"
+        block_type_paragraph = block_to_block_type(paragraph)
+        self.assertEqual(block_type_paragraph, BlockType.PARAGRAPH)
+
+    def test_block_to_block_type_headings(self):
+
+        headings = [
+                "# this is heading",
+                "## this is heading",
+                "### this is heading",
+                "#### this is heading",
+                "##### this is heading",
+                "###### this is heading",
+                ]
+
+        for heading in headings:
+            with self.subTest(heading=heading):
+                self.assertEqual(block_to_block_type(heading), BlockType.HEADING)
+
+
+    def test_block_to_block_type_code(self):
+        code = """```
+this is code
+```"""
+        block_type_code = block_to_block_type(code)
+        self.assertEqual(block_type_code, BlockType.CODE)
+
+    def test_block_to_block_type_code_empty(self):
+        code = "```\n```"
+        block_type_code = block_to_block_type(code)
+        self.assertEqual(block_type_code, BlockType.CODE)
+
+    def test_block_to_block_type_quote(self):
+        quote = """>still\n>quoted"""
+        block_type_quote = block_to_block_type(quote)
+        self.assertEqual(block_type_quote, BlockType.QUOTE)
+
+    def test_block_to_block_type_unordered_list(self):
+        unordered_list = """- something
+- something else
+- something way too big"""
+        block_type_unordered_list = block_to_block_type(unordered_list)
+        self.assertEqual(block_type_unordered_list, BlockType.UNORDERED_LIST)
+
+    def test_block_to_block_type_ordered_list(self):
+        ordered_list = """1. Hello
+2. World
+3. Robot"""
+
+        block_type_ordered_list = block_to_block_type(ordered_list)
+        self.assertEqual(block_type_ordered_list, BlockType.ORDERED_LIST)
