2d5838a959b1b87cf89ca694082a71147f1cff42
Priyanshu Gupta <priyanshoon.pg@gmail.com>
2026-05-17 02:55:41 +0530

    boom boom boom

--- src/main.py
+++ src/main.py
@@ -1,9 +1,34 @@
+import shutil
+import os
+
 from textnode import TextNode, TextType
+
+
+def copy_static_to_public():
+    # delete everything to public first
+    if os.path.exists("public"):
+        shutil.rmtree("public")
+    os.mkdir("public")
+    copy_static_to_public_helper("static", "public")
+
+
+def copy_static_to_public_helper(src, dst):
+    for name in os.listdir(src):
+        src_path = os.path.join(src, name)
+        dst_path = os.path.join(dst, name)
+
+        if os.path.isfile(src_path):
+            print(f"copying... [{src_path} -> {dst_path}]")
+            shutil.copy(src_path, dst_path)
+        else:
+            os.mkdir(dst_path)
+            copy_static_to_public_helper(src_path, dst_path)
 
 
 def main():
     dummy = TextNode("This is my website link", TextType.LINK, "https://priyanshoon.me")
     print(dummy)
+    copy_static_to_public()
 
 
 main()
